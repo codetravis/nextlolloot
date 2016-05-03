7f03fb59d140565e47f741bf2fd27f6e238d684e
Travis Cooper <tcooper@Traviss-MacBook-Pro.local>
2016-05-03 13:54:51 -0400

    reading in and displaying information from API effectively, now need to sort and identify which champion has been close to getting a crate

--- app/controllers/champions_controller.rb
+++ app/controllers/champions_controller.rb
@@ -1,7 +1,24 @@
+require 'open-uri'
+require 'json'
+
 class ChampionsController < ApplicationController
-  def collect
+
+  def new
+
   end
 
   def list
+    # /api/lol/{region}/v1.4/summoner/by-name/{summonerNames}
+    api_key = Rails.application.secrets.rito_api_key
+    summoner_uri = "https://na.api.pvp.net/api/lol/#{params[:region]}/v1.4/summoner/by-name/#{params[:summonername]}?api_key=#{api_key}"
+    test = JSON.parse(open(summoner_uri).read)
+    player_id = test[params[:summonername].downcase]["id"]
+    mastery_uri = "https://na.api.pvp.net/championmastery/location/#{params[:region]}1/player/#{player_id}/champions?api_key=#{api_key}"
+    @masteries = JSON.parse(open(mastery_uri).read).map { |row| [row["championId"], row] }.to_h
+
+    champion_uri = "https://global.api.pvp.net/api/lol/static-data/#{params[:region].downcase}/v1.2/champion?api_key=#{api_key}"
+    champions = JSON.parse(open(champion_uri).read)
+
+    @display_info = champions["data"]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  get 'champions/collect'
 
-  get 'champions/list'
+  resources :summoners
+
+  get 'champions/new' => 'champions#new'
+  get 'champions/list' => 'champions#list'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'champions#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
